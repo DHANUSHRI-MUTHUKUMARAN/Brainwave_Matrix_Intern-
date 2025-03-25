270712fd4383a34e8330d13dea9abe645cedc598
DHANUSHRI-MUTHUKUMARAN <mdhanushri2006@gmail.com>
2025-03-25 22:21:21 +0530

    FINAL ATM.py

--- ATM.py
+++ ATM.py
@@ -4,31 +4,31 @@ class ATM:
     
     def check_balance(self):
         print("*****************")
-        print(f"YOUR BALANCE IS ${self.balance:.2f}")
+        print(f"Your Balance is ${self.balance:.2f}")
         print("*****************")
     
     def deposit(self):
         print("*****************")
-        amount = float(input("ENTER AN AMOUNT TO BE DEPOSITED:"))
+        amount = float(input("Enter an amount to be Deposited: "))
         print("*****************")
         if amount < 0:
             print("*****************")
-            print("THAT'S NOT A VALID AMOUNT")
+            print("That's not a valid amount")
             print("*****************")
         else:
             self.balance += amount
     
     def withdraw(self):
         print("*****************")
-        amount = float(input("ENTER AMOUNT TO BE WITHDRAWN:"))
+        amount = float(input("Enter amount to be Withdrawn: ))
         print("*****************")
         if amount > self.balance:
             print("*****************")
-            print("INSUFFICIENT FUNDS")
+            print("Insufficient funds")
             print("*****************")
         elif amount < 0:
             print("*****************")
-            print("AMOUNT MUST BE GREATER THAN 0")
+            print("Amount must be greater than 0")
             print("*****************")
         else:
             self.balance -= amount
@@ -46,7 +46,7 @@ def atm_interface():
         print("4. Exit")
         print("*****************")
         
-        choice = input("ENTER YOUR CHOICE (1-4):")
+        choice = input("Enter your choice (1-4):")
         
         switch = {
             '1': atm.check_balance,
